b26923bf12529f658df358b1c16b675e6af558e6
goirik <chakrabarty.goirik@gmail.com>
2024-04-30 01:00:30 +0530

    Updated ReadME, modified main.py to accept args, added model url

--- README.md
+++ README.md
@@ -1,7 +1,71 @@
-# ReMOVE: A Reference-free Metric for Object Erasure
+# ReMOVE
 
-![alt text](logo.png "ReMOVE")
+This is the official PyTorch implementation for the CVPRW 24 paper: "ReMOVE: A Reference-free Metric for Object Erasure".
 
-## Description
+![ReMOVE](./assets/logo.png)
 
-Will be updated soon.
+## Requirements
+
+This code was tested with `python=3.8.18`, `pytorch=2.0.1` and `torchvision=0.8.2`. Please follow the instructions [here](https://pytorch.org/get-started/locally/) to install PyTorch and TorchVision dependencies. Installing both PyTorch and TorchVision with CUDA support is strongly recommended.
+
+**NOTE**: This codebase requires SAM to be installed. To install SAM, please follow the instructions [here](https://github.com/facebookresearch/segment-anything/blob/main/README.md#installation).
+
+```
+pip install git+https://github.com/facebookresearch/segment-anything.git
+```
+
+## Getting Started
+
+### Setup ###
+
+Start by downloading the model at `models/url.txt` (~2.4GB). Currently, `main.py` uses the ViT-H model, but other [models](https://github.com/facebookresearch/segment-anything/blob/main/README.md#model-checkpoints) can be used by modifying `L49` of `main.py`.
+
+```
+wget -i models/url.txt -P models/
+```
+
+### Usage ###
+
+The `main.py` script takes the following arguments
+* `--image_path` : Path to the image.
+* `--mask_path` : Path to the corresponding mask.
+* `--crop` (_optional_) : Crops the image using a bounding box around the object mask.
+* `--draw` (_optional_) : Draws a bounding box around the object (saved to `masked.png`) if `--crop` is used.
+
+A set of sample images with objects removed using various methods are provided in `./examples/`.
+
+#### Bad Inpainting
+
+![BAD](./assets/example_1.png)
+
+```
+python main.py -i 'examples/sdinpaint/823000000001.jpg' -m 'examples/masks/823000000001.jpg'
+```
+
+```
+python main.py -i 'examples/sdinpaint/823000000001.jpg' -m 'examples/masks/823000000001.jpg' --crop
+```
+
+#### Good Inpainting
+
+![GOOD](./assets/example_2.png)
+
+```
+python main.py -i 'examples/sdinpaint/122000000009.jpg' -m 'examples/masks/122000000009.jpg'
+```
+
+```
+python main.py -i 'examples/sdinpaint/122000000009.jpg' -m 'examples/masks/122000000009.jpg' --crop
+```
+
+## Citation
+
+If you use ReMOVE or find this work useful for your research, please use the following BibTeX entry.
+```bibtex
+@InProceedings{Chandrasekar_2024_CVPRW,
+    author    = {Chandrasekar$^*$, Aditya and Chakrabarty$^*$, Goirik and Bardhan, Jai and Hebbalaguppe, Ramya and AP, Prathosh},
+    title     = {ReMOVE: A Reference-free Metric for Object Erasure},
+    booktitle = {IEEE/CVF Conference on Computer Vision and Pattern Recognition Workshops (CVPRW)},
+    year      = {2024},
+}
+```
--- main.py
+++ main.py
@@ -7,37 +7,47 @@ from torch.nn.functional import cosine_similarity
 from segment_anything import sam_model_registry
 from segment_anything.predictor import SamPredictor
 from crop import find_smallest_bounding_square, draw_bb
+import argparse
 
-# FileNotFoundError: [Errno 2] No such file or directory: 'sam_vit_h_4b8939.pth'
-# Download the model in root dir from URL - https://huggingface.co/spaces/abhishek/StableSAM/blob/main/sam_vit_h_4b8939.pth
+import warnings
+warnings.filterwarnings("ignore")
 
-sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth").cuda()
-predictor = SamPredictor(sam)
-DATA_DIR = 'examples/sdinpaint'
-MASK_DIR = 'examples/masks'
-bname = '823000000001'          # Bad Inpainting
-# bname = '122000000009'          # Good Inpainting
-crop = True
+def get_score(predictor, args):
+    if args.crop:
+        binary_image = np.array(Image.open(args.mask_path))
+        if args.draw:
+            draw_bb(args.mask_path)
+        x, y, size = find_smallest_bounding_square(binary_image)
+        
+        input_img = np.array(Image.open(args.image_path).convert("RGB"))[y:y+size, x:x+size]
+        
+        mask_fg = np.array(Image.fromarray(np.array(Image.open(args.mask_path).convert("L"))[y:y+size, x:x+size]).resize((64,64))).reshape((1,1,64,64))//255
+        mask_bg = 1 - mask_fg
+        
+    else:
+        input_img = np.array(Image.open(args.image_path).convert("RGB"))
+        
+        mask_fg = np.array(Image.open(args.mask_path).resize((64,64))).reshape((1,1,64,64))//255
+        mask_bg = 1 - mask_fg
+        
+    embeddings = predictor.get_aggregate_features(input_img, [mask_fg, mask_bg])
 
-if crop:
-    binary_image = np.array(Image.open(os.path.join(MASK_DIR, bname+'.jpg')))
-    # draw_bb(os.path.join(MASK_DIR, bname+'.jpg'))
-    x, y, size = find_smallest_bounding_square(binary_image)
-    
-    input = np.array(Image.open(os.path.join(DATA_DIR, bname+'.jpg')).convert("RGB"))[y:y+size, x:x+size]
-    
-    mask_fg = np.array(Image.fromarray(np.array(Image.open(os.path.join(MASK_DIR, bname+'.jpg')).convert("L"))[y:y+size, x:x+size]).resize((64,64))).reshape((1,1,64,64))//255
-    mask_bg = 1 - mask_fg
-    
-else:
-    input = np.array(Image.open(os.path.join(DATA_DIR, bname+'.jpg')))
-    
-    mask_fg = np.array(Image.open(os.path.join(MASK_DIR,bname+'.jpg')).resize((64,64))).reshape((1,1,64,64))//255
-    mask_bg = 1 - mask_fg
-    
-embeddings = predictor.get_aggregate_features(input, [mask_fg, mask_bg])
+    remove_score = cosine_similarity(embeddings[0], embeddings[1]).item()
 
-remove_score = cosine_similarity(embeddings[0], embeddings[1]).item()
+    return remove_score
 
-print("The ReMOVE Score for Quality of Object Erasure is:", remove_score)
+if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(description="arguments for ReMOVE")
+    parser.add_argument("-i" , "--image_path", type=str, help="Path to the image file.")
+    parser.add_argument("-m", "--mask_path", type=str, help="Path to the corresponding mask file.")
+    parser.add_argument("--crop", action="store_true", default=False, help="Crop the image using a bounding box around the mask.")
+    parser.add_argument("--draw", action="store_true", default=False, help="Draws a bounding box around the object if crop is chosen.")
+
+    args = parser.parse_args()
+
+    sam = sam_model_registry["vit_h"](checkpoint="models/sam_vit_h_4b8939.pth").cuda()
+    predictor = SamPredictor(sam)
+
+    remove_score = get_score(predictor, args)
+    print("The ReMOVE Score for Quality of Object Erasure is:", remove_score)
